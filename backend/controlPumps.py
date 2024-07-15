0184d02ba5968081b41b4b629470055c5a55aeb4
Honey-Sekhonm <sekhonhoneypal@gmail.com>
2024-07-14 21:28:42 -0700

    control logic

--- backend/controlPumps.py
+++ backend/controlPumps.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from gpiozero import OutputDevice
 from time import sleep
+import time
 import subprocess
 
 app = Flask(__name__)
@@ -15,6 +16,8 @@ PUMP_PINS = {
     "slot4": 27
 }
 
+Bottle = []
+Duration = []
 # Initialize the relays (pumps)
 pumps = {slot: OutputDevice(pin, active_high=False, initial_value=False) for slot, pin in PUMP_PINS.items()}
 
@@ -27,20 +30,61 @@ def pump_off(pump):
     print(f"Pump on GPIO {pump.pin.number} is OFF")
 
 def control_pump(slot, duration):
-    if slot in pumps:
-        pump = pumps[slot]
-        pump_on(pump)
-        sleep(duration)
-        pump_off(pump)
-    else:
-        print(f"Invalid slot: {slot}")
+            flag = 0
+            start_timer = time.time()
+            if(Duration[0]==0):
+                pump_off(pumps[0])
+            else:
+                pump_off(pumps[0])
+                flag = flag + 1
+            if(Duration[1]==0):
+                pump_off(pumps[1])
+            else:
+                pump_off(pumps[1])
+                flag = flag + 1
+
+            if(Duration[2]==0):
+                pump_off(pumps[2])
+            else:
+                pump_off(pumps[2])
+                flag = flag + 1
+
+            if(Duration[3]==0):
+                pump_off(pumps[3])
+            else:
+                pump_off(pumps[3])
+                flag = flag + 1
+
+            while(True):
+                if(time.time()-start_timer>=Duration[0]):
+                    pump_off(pumps[0])
+                    flag = flag - 1
+                if(time.time()-start_timer>=Duration[1]):
+                    pump_off(pumps[1])
+                    flag = flag - 1
+                if(time.time()-start_timer>=Duration[2]):
+                    pump_off(pumps[2])
+                    flag = flag - 1
+                if(time.time()-start_timer>=Duration[3]):
+                    pump_off(pumps[3])
+                    flag = flag - 1
+                
+                if(flag==0):
+                     break
 
 @app.route('/control_pumps', methods=['POST'])
 def control_pumps():
     data = request.json
     for slot, amount in data.items():
-        duration = amount * 1.25  # 1.25 seconds per ml
+        duration = amount * 1.15  # 1.25 seconds per ml
+        Duration.append(duration)
+        Bottle.append(slot)
+        print("Duration: ", Duration)
+        print("Bottle:", Bottle)
+    
+
         control_pump(slot, duration)
+        
     return jsonify({"status": "success"})
 
 @app.route('/poweroff', methods=['POST'])
